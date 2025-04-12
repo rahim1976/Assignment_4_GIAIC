5e980e319d746820eed41aa8cd227ec9c30eee99
Rahim Ali <rahimali1967@icloud.com>
2025-04-13 02:12:03 +0500

    Create 04_how_old_are_they.py

---
+++ Assignment_00_To_07/00_Intro_Python/04_how_old_are_they.py
@@ -0,0 +1,15 @@
+def main():
+    Anton = 21
+    Beth = 21 + 6 
+    Chen = Beth + 20
+    Drew = Chen + Anton
+    Ethan = Chen
+    
+    print(f'Anton is {Anton} Years Old')
+    print(f'Beth is {Beth} Years Old')
+    print(f'Chen is {Chen} Years Old')
+    print(f'Drew is {Drew} Years Old')
+    print(f'Ethan is {Ethan} Years Old')
+    
+if __name__ == "__main__":
+    main()
