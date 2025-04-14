281aba03ef784fcf1f256f239ddc437a7a0c41d1
Rahim Ali <rahimali1967@icloud.com>
2025-04-14 17:58:14 +0500

    Create 06_seconds_in_year.py

---
+++ Assignment_00_To_07/01_Expressions/06_seconds_in_year.py
@@ -0,0 +1,13 @@
+DAYS_IN_YEAR = 365
+HOURS_PER_DAY = 24
+MINUTES_PER_HOUR = 60
+SECONDS_PER_HOUR = 60
+
+def main():
+   
+    SECONDS_IN_YEAR =  (DAYS_IN_YEAR * HOURS_PER_DAY * MINUTES_PER_HOUR * SECONDS_PER_HOUR)
+   
+    print(f'\nThere Are {SECONDS_IN_YEAR} Seconds In Year')
+    
+if __name__ == '__main__':
+    main()
