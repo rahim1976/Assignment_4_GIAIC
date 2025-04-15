eec0ed9c66d851169824c02ed0def251bdc9e1cc
Rahim Ali <rahimali1967@icloud.com>
2025-04-15 18:35:04 +0500

    create 08_shorten.py & update 07_get_list.py

--- Assignment_00_To_07/02_lists/07_get_list.py
+++ Assignment_00_To_07/02_lists/07_get_list.py
@@ -1,10 +1,10 @@
 def main():
     lst = []  # Empty list
 
-    val = input("Enter a value: ")  # First value
+    val = input("\033[34mEnter a value: \033[0m")  # First value 
     while val:  # While the user input isn't an empty value
         lst.append(val) 
-        val = input("Enter a value: ")  # For upcoming values
+        val = input("\033[34mEnter a value: \033[0m")  # For upcoming values 
 
     print("Here's the list:", lst)
 
