bc66efaa674e6f3862e421c1dcd66350c606a360
Rahim Ali <rahimali1967@icloud.com>
2025-04-17 18:05:10 +0500

    Create 03_wholesome_machine.py

---
+++ Assignment_00_To_07/05_loops_control_flow/03_wholesome_machine.py
@@ -0,0 +1,18 @@
+AFFIRMATION : str = "I am capable of doing anything I put my mind to."
+
+def main():
+    print(f"Please type the following affirmation: {AFFIRMATION}")
+
+    user_feedback = input()  
+    while user_feedback != AFFIRMATION: 
+
+        print("That was not the affirmation.")
+
+        print(f"Please type the following affirmation: {AFFIRMATION}")
+
+        user_feedback = input()
+
+    print("That's right! :)")
+
+if __name__ == '__main__':
+    main()
